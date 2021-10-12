95e22d03e6edaadac3a883c1a92d9377e4821bb6
Stephanie Albanese <stephanie@Stephanies-iMac.local>
2021-10-12 15:29:07 -0400

    create and save person function with error handler in save method

--- myApp.js
+++ myApp.js
@@ -17,7 +17,15 @@ const personSchema = new Schema({
 const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const felicity = new Person({
+    name: "Felicity",
+    age: 18,
+    favoriteFoods: ["Lucky Charms", "popcorn", "Brussels sprouts"],
+  });
+  felicity.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
